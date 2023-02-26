8df2a8a00bb4574acc7bd674a5408388fd90c75b
Efim Shumkov <47829068+E1Georg@users.noreply.github.com>
2023-02-26 15:47:32 +0300

    Add files via upload

---
+++ Infrastructure/Records.Persistence/EntityTypeConfigurations/ResultConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Records.Domain;
+
+namespace Records.Persistence.EntityTypeConfigurations
+{
+    public class ResultConfiguration : IEntityTypeConfiguration<Result>
+    {
+        public void Configure(EntityTypeBuilder<Result> builder)
+        {
+            builder.HasKey(result => result.Id);
+            builder.HasIndex(result => result.Id).IsUnique();
+        }
+
+    }
+}
---
+++ Infrastructure/Records.Persistence/EntityTypeConfigurations/ValueConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Records.Domain;
+
+namespace Records.Persistence.EntityTypeConfigurations
+{
+    public class ValueConfiguration : IEntityTypeConfiguration<Value>
+    {
+        public void Configure(EntityTypeBuilder<Value> builder)
+        {
+            builder.HasKey(value => value.Id);
+            builder.HasIndex(value => value.Id).IsUnique();
+        }
+
+    }
+}
