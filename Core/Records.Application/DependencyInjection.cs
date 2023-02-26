c0b9d94641ad384e4094ed08d80bfe1512650652
Efim Shumkov <47829068+E1Georg@users.noreply.github.com>
2023-02-26 15:44:29 +0300

    Add files via upload

---
+++ Core/Records.Application/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using MediatR;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using Records.Application.Common.Behaviors;
+
+
+namespace Records.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssemblies(new[] { Assembly.GetExecutingAssembly() });
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+            return services;
+        }
+    }
+}
