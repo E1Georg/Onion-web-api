a6b036d92241f948d05d49a98ce49cf7d4f59c22
Efim Shumkov <47829068+E1Georg@users.noreply.github.com>
2023-02-26 15:40:23 +0300

    Add files via upload

---
+++ Core/Records.Application/Interfaces/IResultsDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Records.Domain;
+
+namespace Records.Application.Interfaces
+{
+    public interface IResultsDbContext
+    {
+        DbSet<Result> Results { get; set; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
---
+++ Core/Records.Application/Interfaces/IValuesDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Records.Domain;
+
+namespace Records.Application.Interfaces
+{
+    public interface IValuesDbContext
+    {
+        DbSet<Value> Values { get; set; }       
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
