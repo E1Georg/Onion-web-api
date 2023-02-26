b24ea6fa1f26a23084ce3ad7888c2b38fa59b30c
Efim Shumkov <47829068+E1Georg@users.noreply.github.com>
2023-02-26 15:45:33 +0300

    Add files via upload

---
+++ Core/Records.Domain/Result.cs
@@ -0,0 +1,16 @@
+﻿namespace Records.Domain
+{
+    public class Result
+    {
+        public Guid Id { get; set; }
+        public long allTime { get; set; }
+        public DateTime minValueDatetime { get; set; }
+        public double averageTime { get; set; }
+        public double averageIndicator { get; set; }
+        public double medianaOfIndicator { get; set; }
+        public float maxValueOfIndicator { get; set; }
+        public float minValueOfIndicator { get; set; }
+        public int countOfString { get; set; }
+        public string filename { get; set; }
+    }
+}
---
+++ Core/Records.Domain/Value.cs
@@ -0,0 +1,11 @@
+﻿ namespace Records.Domain
+{
+    public class Value
+    {
+        public Guid Id { get; set; }
+        public DateTime dateTime { get; set; }
+        public int timeInt { get; set; }
+        public float timeFloat { get; set; }
+        public string filename { get; set; }
+    }
+}
